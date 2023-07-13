1ac7f93a16faae7ef6db9a35e5fccb3e5b7a765e
mgfalzon <mattgfalzon@gmail.com>
2023-07-12 20:54:00 -0400

    beef up example/starter code

--- index.ts
+++ index.ts
@@ -7,8 +7,21 @@ const { DEBUG } = process.env;
   const page = await browser.newPage();
 
   await page.goto("https://www.google.com");
+
   const searchbar = page.locator("[title='Search']");
   await searchbar.type("Hello World!");
+  await searchbar.press("Enter");
+
+  const links = page.locator("#search").getByRole("link");
+  await links.first().click();
+
+  /**
+   * Waiting for demo purposes, you should never wait.
+   * 
+   * Tests that wait for time are inherently flaky (can ocassionally fail).
+   * Use Locator actions and web assertions that wait automatically.
+  */
+  await page.waitForTimeout(1000);
 
   await browser.close();
 })();
